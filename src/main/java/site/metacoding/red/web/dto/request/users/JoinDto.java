e8fea0f3b0101f87c6f1860c8508343aaff35db3
sdics <jhnadas@naver.com>
2022-09-02 21:48:43 +0900

    20220902 2100 State

---
+++ src/main/java/site/metacoding/red/web/dto/request/users/JoinDto.java
@@ -0,0 +1,13 @@
+package site.metacoding.red.web.dto.request.users;
+
+import lombok.Getter;
+import lombok.Setter;
+
+	@Setter
+	@Getter
+	public class JoinDto {
+		private String username;
+		private String password;
+		private String email;
+	}
+
